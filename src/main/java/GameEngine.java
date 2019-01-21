ac308ab8f9516ffbc5b64b26c39429db562655c4
Ryuji Ogawa <e185736@ie.u-ryukyu.ac.jp>
2019-01-21 21:26:43 +0900

    レポート６

--- src/main/java/GameEngine.java
+++ src/main/java/GameEngine.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+import java.util.Random;
 public class GameEngine {
+    public static void GameEngine() {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        int answer = random.nextInt(1000);
+        System.out.print("何か入力してください");
+        int count = 0;
+        while (count < 10) {
+            int number = scan.nextInt();
+            if (number == answer) {
+                System.out.println("ゲームクリア！");
+                count = 10;
+            }
+            if (number < answer) {
+                System.out.println("答えはもっと大きいです");
+                count++;
+            } else {
+                System.out.println("答えはもっと小さいです");
+                count++;
+            }
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,2 +1,17 @@
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
 public class main {
+    public static void main(String[] args){
+        GameEngine gameEngine = new GameEngine();
+        gameEngine.GameEngine();
+
+
+
+    }
+
+
+
 }
+
